35d8c2afc1cfd71edb4f92330843ef23c7f494c7
jsmz <37253121+jsmzdf@users.noreply.github.com>
2020-02-22 20:50:26 -0500

    Update EjerciciosBasica.py

--- EjerciciosBasica.py
+++ EjerciciosBasica.py
@@ -107,6 +107,15 @@ def pot(num,exp):
     if exp ==1: return num
     return pot(num,exp-1)*num
 print('la potencia'+pot(base,exponente))
+#
+imgresodenumfob=int(input('ingrese un numero para calcular fibo'))
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1) + fib(n-2)
+print('El numero de la serie de fibo: ',fib(imgresodenumfob))
 #################################################################################modulos
 #suma resta dicision potencia enteres
 import modulo1
